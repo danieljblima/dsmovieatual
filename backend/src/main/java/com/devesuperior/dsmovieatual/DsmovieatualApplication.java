b3481579ac93b48416dd4ba2b653165bfad9b4c6
danieljblima <djblima2@gmail.com>
2022-03-10 06:16:47 -0300

    Project created

---
+++ backend/src/main/java/com/devesuperior/dsmovieatual/DsmovieatualApplication.java
@@ -0,0 +1,13 @@
+package com.devesuperior.dsmovieatual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmovieatualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmovieatualApplication.class, args);
+	}
+
+}
